29fe1640c8a35b37d65677c97c5acf59becbc66d
Wahaj Rehman <wahaj.rehman@hrsgbpo.com>
2022-08-31 16:52:49 +0500

    pop

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function createWindow() {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('./pages/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
